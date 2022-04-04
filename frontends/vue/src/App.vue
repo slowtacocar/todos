eb95c969c5f6a1f32723e6c90ea64d249961b1c3
Bobby George <slowtacocar@gmail.com>
2022-04-03 20:27:35 -0400

    Add Vue frontend

---
+++ frontends/vue/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Todo from "./components/Todo.vue";
+import { ref, onMounted } from "vue";
+import { addTodo, getTodos } from "todos-client";
+
+const data = ref(null);
+const error = ref(null);
+const text = ref("");
+
+function mutate() {
+  getTodos()
+    .then((d) => (data.value = d))
+    .catch((e) => (error.value = e));
+}
+
+function handleAdd() {
+  addTodo({ text: text.value, done: false })
+    .then(mutate)
+    .catch(() => {
+      alert("Failed to add");
+    });
+}
+
+onMounted(mutate);
+</script>
+
+<template>
+  <p v-if="error">An error occurred while fetching data</p>
+  <p v-else-if="!data">Loading...</p>
+  <template v-else>
+    <h1>To Do</h1>
+    <h2>Remaining Tasks</h2>
+    <ul>
+      <li v-for="todo in data">
+        <Todo :todo="todo" @on-change="mutate" />
+      </li>
+    </ul>
+    <h2>New Task</h2>
+    <form @submit.prevent="handleAdd">
+      <input v-model="text" required />
+      <button type="submit">Add</button>
+    </form>
+  </template>
+</template>
---
+++ frontends/vue/src/components/Todo.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { deleteTodo, updateTodo } from "todos-client";
+import { ref } from "vue";
+
+const props = defineProps(["todo"]);
+const emit = defineEmits(["onChange"]);
+const editing = ref(false);
+const text = ref(props.todo.text);
+
+function handleUpdate() {
+  updateTodo(props.todo.id, { text: text.value })
+    .then(() => emit("onChange"))
+    .catch(() => {
+      alert("Failed to update");
+    });
+  editing.value = false;
+}
+
+function handleCheck(event) {
+  updateTodo(props.todo.id, { done: event.target.checked })
+    .then(() => emit("onChange"))
+    .catch(() => {
+      alert("Failed to check");
+    });
+}
+
+function handleDelete() {
+  deleteTodo(props.todo.id)
+    .then(() => emit("onChange"))
+    .catch(() => {
+      alert("Failed to delete");
+    });
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="handleUpdate" v-if="editing">
+      <input v-model="text" required />
+      <button type="submit">Update</button>
+    </form>
+    <div v-else>
+      <input
+        :id="todo.id"
+        type="checkbox"
+        :checked="todo.done"
+        @change="handleCheck"
+      />
+      <label :for="todo.id">{{ todo.text }}</label>
+    </div>
+
+    <button @click="editing = !editing">
+      {{ editing ? "Cancel" : "Edit" }}
+    </button>
+    <button @click="handleDelete">Delete</button>
+  </div>
+</template>
